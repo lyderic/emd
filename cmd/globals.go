5759a9213b708e9a6481e624ae50bf67e87790a0
lyderic <lyderic@blulaab.lyderic.net>
2024-08-07 11:50:10 +0200

    0.1.1: bumped up deps versions (glamour 0.8.0)

--- cmd/globals.go
+++ cmd/globals.go
@@ -2,7 +2,7 @@ package cmd
 
 const (
 	PROGNAME    = "emd"
-	VERSION     = "0.1.0"
+	VERSION     = "0.1.1"
 	PAGER       = "less" /* more doesn't work */
 	DESCRIPTION = "markdown viewer for the command line"
 )
