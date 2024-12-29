61cba13c9f831a348836733a918bda2a032d57c4
lyderic <lyderic@balzac.lyderic.net>
2024-12-29 19:16:12 +0100

    0.1.6: bumped up deps

--- cmd/globals.go
+++ cmd/globals.go
@@ -2,7 +2,7 @@ package cmd
 
 const (
 	PROGNAME    = "emd"
-	VERSION     = "0.1.5"
+	VERSION     = "0.1.6"
 	PAGER       = "less" /* more doesn't work */
 	DESCRIPTION = "markdown viewer for the command line"
 )
