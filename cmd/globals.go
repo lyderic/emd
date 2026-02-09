9c9fe49c0d6963f44610660135b9231967068637
lyderic <lyderic@balzac.lyderic.net>
2026-02-09 14:21:07 +0100

    0.2.0: better numbering for tagging (more sequential)

--- cmd/globals.go
+++ cmd/globals.go
@@ -2,7 +2,7 @@ package cmd
 
 const (
 	PROGNAME    = "emd"
-	VERSION     = "0.1.10"
+	VERSION     = "0.2.0"
 	PAGER       = "less" /* more doesn't work */
 	DESCRIPTION = "markdown viewer for the command line"
 )
